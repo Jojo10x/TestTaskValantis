8c096ff425c701702040e20c0b0b3c55fcd1c53d
Joel Kasisi <joelkasisi10@gmail.com>
2024-02-29 16:57:13 +0300

    css for product

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import "./App.css"
 
 const Product = ({ product }) => (
-  <li>
-    {product.product} {product.price} {product.brand}
+  <li className="product-item">
+    <div className="product-details">
+      <div className="product-info">
+        <h2 className="product-name"> {product.product}</h2>
+        <p className="product-price">${product.price} </p>
+      </div>
+      <p className="product-brand"> {product.brand}</p>
+    </div>
   </li>
 );
 
